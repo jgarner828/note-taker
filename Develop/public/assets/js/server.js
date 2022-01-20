6ecd200b5719e5c5ab42530522f2ce2978c7565f
Justin Garner <j.garner828@gmail.com>
2022-01-20 09:17:50 -0500

    created basic routing framework. need to update arrow functions in routing

--- Develop/public/assets/js/server.js
+++ Develop/public/assets/js/server.js
@@ -5,26 +5,43 @@ const db = require('../../../db/db.json');
 const app = express();
 const PORT = 3001;
 const path = require('path');
+const { response } = require('express');
 
 
 app.use(express.static('public'));
 
 
-app.get('/notes', (req, res) => res.sendFile(path.join(__dirname, '../../notes.html')));
+// GET /notes should return the notes.html file.
+app.get('/notes', (req, res) => { 
+  res.sendFile(path.join(__dirname, '../notes.html'))
+});
 
-app.get('*', (req, res) => res.sendFile(path.join(__dirname, '../../index.html')));
+
+// GET /api/notes should read the db.json file and return all saved notes as JSON.
+app.get('/api/notes', (req, res) => {
+  console.log(req)
+  res.status(200).json(db);
+});
 
 
-app.get('/api/notes', (req, res) => {}
-    // * `GET /api/notes` should read the `db.json` file and return all saved notes as JSON.
-);
-
+// POST /api/notes should receive a new note to save on the request body, add it to the db.json file, and then return the new note to the client. You'll need to find a way to give each note a unique id when it's saved (look into npm packages that could do this for you).
 app.post('/api/notes', (req, rec) => {
-  // * `POST /api/notes` should receive a new note to save on the request body, add it to the `db.json` file, and then return the new note to the client. You'll need to find a way to give each note a unique id when it's saved (look into npm packages that could do this for you).
 
-})
+});
 
 
-app.listen(PORT, () =>
-  console.log(`Serving static asset routes on port ${PORT}!`)
-);
+
+// DELETE /api/notes/:id should receive a query parameter that contains the id of a note to delete. To delete a note, you'll need to read all notes from the db.json file, remove the note with the given id property, and then rewrite the notes to the db.json file.
+app.delete('/api/notes:id', function (req, res) {
+  res.send('DELETE request to homepage')
+});
+
+
+// GET * should return the index.html file.
+app.get('*', (req, res) =>  { 
+  res.sendFile(path.join(__dirname, '../index.html'))
+});
+
+
+// listen to the port
+app.listen(PORT, () => console.log(`Serving static asset routes on port ${PORT}!`));
