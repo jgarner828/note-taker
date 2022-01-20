f37f9c289527340a732e6e82a68560c6ca37ff7d
Justin Garner <j.garner828@gmail.com>
2022-01-20 17:12:20 -0500

    fixed delete and post api routing

--- Develop/public/assets/js/server.js
+++ Develop/public/assets/js/server.js
@@ -26,6 +26,7 @@ app.use(express.urlencoded({ extended: true })) // for parsing application/x-www
 
 // GET /notes should return the notes.html file.
 app.get('/notes', (req, res) => {
+
   console.log(`${req.method} request from ${req.ip}`); 
   res.sendFile(path.join(__dirname, '../notes.html'))
 });
@@ -35,6 +36,7 @@ app.get('/notes', (req, res) => {
 
 // GET /api/notes should read the db.json file and return all saved notes as JSON.
 app.get('/api/notes', (req, res) => {
+
   console.log(`${req.method} request from ${req.ip}`);
   res.status(200).json(db);
 });
@@ -42,21 +44,43 @@ app.get('/api/notes', (req, res) => {
 
 
 
-// TODO: POST /api/notes should receive a new note recieved as 'req.body = JSON.stringify(object)' to save on the request body, add it to the db.json file, and then return the new note to the client. give each note a unique id when it's saved using UUID ).
+// POST /api/notes should receive a new note received as 'req.body = JSON.stringify(object)' to save on the request body, add it to the db.json file, and then return the new note to the client. give each note a unique id when it's saved using UUID ).
 app.post('/api/notes', (req, res) => {
+
   console.log(`${req.method} request from ${req.ip}`);
 
   let { title, text } = req.body;
-  let uuid = uuidv4();
 
   if (title && text) {
-    const newNote = {
-      title,
-      text,
-      uuid
-    }
+
+        // read the file
+    fs.readFile('../../../db/db.json', 'utf8', (err, data) => {
+
+      if (err) {
+          console.log(`Error reading file from disk: ${err}`);
+      } else {
+          // parse JSON string to JSON object
+          const databases = JSON.parse(data);
+
+          // add a new record
+          databases.push({
+              title,
+              text,
+              id: uuidv4()
+          });
+
+          // write new data back to the file
+          fs.writeFile('../../../db/db.json', JSON.stringify(databases, null, 4), (err) => {
+              if (err) {
+                  console.log(`Error writing file: ${err}`);
+              }
+          });
+      }
+    });
+
   }
 
+  res.send('POST request sent.');
 
 });
 
@@ -65,18 +89,49 @@ app.post('/api/notes', (req, res) => {
 
 // TODO: DELETE /api/notes/:id should receive a query parameter that contains the id of a note to delete. To delete a note, you'll need to read all notes from the db.json file, remove the note with the given id property, and then rewrite the notes to the db.json file.
 app.delete('/api/notes:id', function (req, res) {
+
   console.log(`${req.method} request from ${req.ip}`);
 
-  res.send('DELETE request to /api/notes/:id')
+  let url = req.originalUrl;
+  let urlSplit = url.split(':');
+  let id = urlSplit[1];
+  console.log(id);
+
+
+  fs.readFile('../../../db/db.json', 'utf8', (err, data) => {
+
+    if (err) {
+        console.log(`Error reading file from disk: ${err}`);
+    } else {
+
+        // parse JSON string to JSON object
+        const databases = JSON.parse(data);
+
+        let updatedDb = databases.filter( note => {
+          return note.id !== id;
+        });
+
+        // match and delete id
+        res.send('DELETE method');
+
+        // write new data back to the file
+        fs.writeFile('../../../db/db.json', JSON.stringify(updatedDb, null, 4), (err) => {
+            if (err) {
+                console.log(`Error writing file: ${err}`);
+            }
+        });
+    }
+  });
+
 });
 
 
 
 
 // GET * should return the index.html file.
-app.get('*', (req, res) =>  { 
-  console.log(`${req.method} request from ${req.ip}`);
+app.get('*', (req, res) =>  {
 
+  console.log(`${req.method} request from ${req.ip}`);
   res.sendFile(path.join(__dirname, '../index.html'))
 });
 
@@ -85,3 +140,4 @@ app.get('*', (req, res) =>  {
 
 // listen to the port
 app.listen(PORT, () => console.log(`Serving static asset routes on port ${PORT}!`));
+
