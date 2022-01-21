1ab354b3cf88ed20688b48468a0261b793ce5c8b
Justin Garner <j.garner828@gmail.com>
2022-01-21 10:10:30 -0500

    reorganized constants in server.js

--- server.js
+++ server.js
@@ -1,14 +1,12 @@
 const express = require('express');
 const fs = require('fs');
 const { v4: uuidv4 } = require('uuid');
-
+const path = require('path');
 
 const db = require('./db/db.json');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
-const path = require('path');
-const { parse } = require('path/posix');
 
 
 app.use(express.static('public')); //for static public folder
